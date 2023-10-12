8f186b517a135dbdcb8d9dbcdac292a788dbc57f
Ardiansyah Latif <ardiansyahlatif6420@gmail.com>
2023-10-12 16:17:50 +0800

    change dataset folder path

--- TopLevel/FaceRecognitionTopLevel.py
+++ TopLevel/FaceRecognitionTopLevel.py
@@ -7,6 +7,8 @@ from FaceDetectionModel.MTCNNModel import MTCNNModel
 from FaceDetectionModel.HaarCascadeModel import HaarCascadeModel
 from FaceDetectionModel.HOGModel import HOGModel
 
+from HumanDetector import HumanDetector
+
 from deepface import DeepFace
 import logging, pickle, numpy as np
 
@@ -31,34 +33,41 @@ class FaceRecognitionTopLevel(tk.Toplevel):
     self.cap = cv2.VideoCapture(0)
     self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
     self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    self.left_frame = tk.Frame(self)
+    self.left_frame.pack(fill="both", expand=True, side="left")
     
     # load face recognition model
     with open('SVM_Model/svm_model.pkl', 'rb') as model_file:
       self.model = pickle.load(model_file)
 
-    self.webcam_label = tk.Label(self)
+    # self.humanDetector = HumanDetector()
+    self.webcam_label = tk.Label(self.left_frame)
     self.webcam_label.pack(fill="both", expand=True, side="top")
 
-    self.bottom_frame = tk.Frame(self)
-    self.bottom_frame.pack(fill="y", side="top", pady=(0, 25))
+    self.setting_frame = tk.Frame(self.left_frame)
+    self.setting_frame.pack(fill="y", side="top", pady=(0, 25))
 
     self.selected_model = tk.StringVar()
     self.selected_model.trace("w", self.update_detector)
 
-    self.detection_model_label = tk.Label(self.bottom_frame, text='model pendeteksi wajah')
+    self.detection_model_label = tk.Label(self.setting_frame, text='model pendeteksi wajah')
     self.detection_model_label.grid(column=0, row=1, padx=5, pady=5)
-    self.detection_model_options = ttk.OptionMenu(self.bottom_frame, self.selected_model,
+    self.detection_model_options = ttk.OptionMenu(self.setting_frame, self.selected_model,
                                                   detection_model_options[0], *detection_model_options)
     self.detection_model_options.grid(column=1, row=1, padx=5, pady=5)
 
-    self.threshold_label = tk.Label(self.bottom_frame, text='threshold maksimal cosine similarity')
+    self.threshold_label = tk.Label(self.setting_frame, text='threshold maksimal cosine similarity')
     self.threshold_label.grid(column=0, row=2, padx=5, pady=5)
+
+    self.isVerified = False
+    self.after_id = None
 
     self.threshold = tk.DoubleVar()
     configuration['max_threshold'] = 0.1
     self.threshold.set(0.1)
 
-    self.threshold_entry = tk.Entry(self.bottom_frame, textvariable=self.threshold)
+    self.threshold_entry = tk.Entry(self.setting_frame, textvariable=self.threshold)
     self.threshold_entry.grid(column=1, row=2, pady=5, padx=5)
 
     self.threshold.trace("w", self.update_threshold)
@@ -87,68 +96,63 @@ class FaceRecognitionTopLevel(tk.Toplevel):
       self.face_detector = MTCNNModel()
     elif detector_model == "hog":
       self.face_detector = HOGModel()
+
+  def box_detected_image(self, rgb_image, bounding_box):
+    (x, y, w, h) = bounding_box
+    start_point = (x, y)
+    end_point = (x + w, y + h)
+    rgb_image = cv2.rectangle(rgb_image, start_point, end_point, (255, 0, 0), 2)
+  
+  def get_detected_image(self, bgr_image, bounding_box):
+    (x, y, w, h) = bounding_box
+    start_point = (x, y)
+    end_point = (x + w, y + h)
+    detected_image = bgr_image[start_point[1]:end_point[1], start_point[0]:end_point[0]]
+    return detected_image
+  
+  def identify_detected_image(self, rgb_image, detected_image):
+    image_embedding = get_face_embedding(detected_image)
+    arr_image_embedding = [image_embedding]
+    label = self.model.predict(arr_image_embedding)[0]
+    probabilities = self.model.predict_proba(arr_image_embedding)[0]
+    label_probability = f"{(np.max(probabilities) * 100):.3f}"
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    color = (204, 0, 0)
+    thickness = 0
+    cv2.putText(rgb_image, f'identitas : {label}', (50, 50), font, font_scale, color, thickness)
+    cv2.putText(rgb_image, f'probability : {label_probability}%', (25, 25), font, font_scale, color, thickness)
+
+  def set_image_in_label(self, rgb_image):
+      # Mengkonversi gambar ke PhotoImage``
+      rgba_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2RGBA)
+      rgb_array = rgb_array = Image.fromarray(rgba_image)
+      photo_image = ImageTk.PhotoImage(image = rgb_array)
+      self.webcam_label.photo_image = photo_image
+      self.webcam_label.configure(image=photo_image)
     
   def show_webcam(self):
     _, frame = self.cap.read()
     bgr_image = frame = cv2.flip(frame, 1)
-
-    rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    modified_rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
 
     face_results = self.face_detector.detectMultipleFaces(bgr_image)
+    # jika wajah terdeteksi
+
     if len(face_results) > 0:
-      face_result = face_results[0]
-      (x, y, w, h) = face_result
-      start_point = (x, y)
-      end_point = (x + w, y + h)
-      rgb_image = cv2.rectangle(rgb_image, start_point, end_point, (255, 0, 0), 2)
+      bounding_box = face_results[0]
+      # method ini memodifikasi isi variabel modified_rgb_image
+      detected_image = self.get_detected_image(bgr_image, bounding_box)
+      self.box_detected_image(modified_rgb_image, bounding_box)
+      # method ini memodifikasi isi variabel modified_rgb_image
+      self.identify_detected_image(modified_rgb_image, detected_image)      
 
-      
-      detected_image = bgr_image[start_point[1]:end_point[1], start_point[0]:end_point[0]]
-      # dataset_path = "DATASET/"
-      # dfs = DeepFace.find(detected_image, dataset_path, enforce_detection=False)
+      self.set_image_in_label(modified_rgb_image)
+      self.after_id = self.after(10, self.show_webcam)
 
-      image_embedding = get_face_embedding(detected_image)
-      arr_image_embedding = [image_embedding]
-      label = self.model.predict(arr_image_embedding)[0]
-      probabilities = self.model.predict_proba(arr_image_embedding)[0]
-      label_probability = f"{(np.max(probabilities) * 100):.3f}"
-
-      font = cv2.FONT_HERSHEY_SIMPLEX
-      font_scale = 1
-      color = (204, 0, 0)
-      thickness = 0
-      cv2.putText(rgb_image, f'identitas : {label}', (50, 50), font, font_scale, color, thickness)
-      cv2.putText(rgb_image, f'probability : {label_probability}%', (25, 25), font, font_scale, color, thickness)
-
-      # try:
-      #   if (len(dfs) > 0):
-      #     df = dfs[0].iloc[0]
-      #     similar_image_path = df['identity']
-      #     cosine_similarity = df['VGG-Face_cosine']
-      #     label = os.path.basename(os.path.dirname(similar_image_path))
-      #     font = cv2.FONT_HERSHEY_SIMPLEX
-      #     font_scale = 1
-      #     color = (204, 0, 0)
-      #     thickness = 0
-      #     origin_identitas = (50, 50)
-      #     print(f"cosine similarity : {cosine_similarity}")
-      #     if (cosine_similarity > configuration['max_threshold']):
-      #       cv2.putText(rgb_image, f'wajah tidak dikenali', origin_identitas, font, font_scale, color, thickness)
-      #     else:
-      #       cv2.putText(rgb_image, f'identitas : {label}', origin_identitas, font, font_scale, color, thickness)
-      # except IndexError:
-      #   pass
-
-    # Mengkonversi gambar ke PhotoImage
-    rgba_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2RGBA)
-    rgb_array = rgb_array = Image.fromarray(rgba_image)
-    photo_image = ImageTk.PhotoImage(image = rgb_array)
-    self.webcam_label.photo_image = photo_image
-    self.webcam_label.configure(image=photo_image)
-    self.after_id = self.webcam_label.after(10, self.show_webcam)
-    
   def process_destroy(self):
-    self.webcam_label.after_cancel(self.after_id)
+    self.after_cancel(self.after_id)
     self.destroy()
     self.cap.release()
     cv2.destroyAllWindows()
